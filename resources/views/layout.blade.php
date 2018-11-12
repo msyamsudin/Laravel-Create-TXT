e6f75b31e2c0bb558082cffc181dbd78df0b7476
msyamsudin <msyamsudin1994@gmail.com>
2018-11-12 11:05:21 +0700

    Tambah Navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,12 +18,28 @@
 
 </head>
 <body>
-    <br>
-        <div class="container btn-group">
-            <a href="{{ url('pengguna') }}" class="btn btn-primary">Beranda</a>
-            <a href="{{ url('pengguna/create') }}" class="btn btn-primary">Tambah Data</a>
-        </div>
-    <br>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="{{ url('pengguna') }}">MSN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('pengguna') }}">Beranda</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('pengguna/create') }}">Tambah Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lain-lain</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+
     <div class="container">
       @yield('konten')
     </div>
