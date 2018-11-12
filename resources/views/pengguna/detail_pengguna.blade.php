0752b80f1d447c5b575bb2e616ef070c00dc5493
msyamsudin <msyamsudin1994@gmail.com>
2018-11-12 13:00:31 +0700

    modify view

--- resources/views/pengguna/detail_pengguna.blade.php
+++ resources/views/pengguna/detail_pengguna.blade.php
@@ -10,33 +10,46 @@ Detail pengguna
     <br />
     <div class="container col-md-6">
     <form>
-      <div class="form-group">
-        <label>Nama</label>
-        <input type="text" class="form-control" name="inname" id="inname" value="{{ $data[0]['innameonly'] }}" readonly>
+      <div class="form-group row">
+        <label class="col-sm-2">Nama</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control " name="inname" id="inname" value="{{ $data[0]['innameonly'] }}" readonly>
+          </div>
      </div>
 
-     <div class="form-group">
-      <label>Email</label>
-      <input type="email" class="form-control" name="inemail" id="inemail" value="{{ $data[0]['inemail'] }}" readonly>
+     <div class="form-group row">
+      <label class="col-sm-2">Email</label>
+        <div class="col-sm-10">
+          <input type="email" class="form-control" name="inemail" id="inemail" value="{{ $data[0]['inemail'] }}" readonly>
+        </div>
      </div>
 
-     <div class="form-group">
-        <label>Date of birth</label>
-        <input type="text" class="form-control indatepicker" name="intgl_lahir" id="intgl_lahir" value="{{ $data[0]['intgl_lahir'] }}" readonly>
+     <div class="form-group row">
+        <label class="col-sm-2">Date of birth</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control indatepicker" name="intgl_lahir" id="intgl_lahir" value="{{ $data[0]['intgl_lahir'] }}" readonly>
+          </div>
      </div>
 
-     <div class="form-group">
-        <label>Nomor</label>
-        <input type="number" class="form-control" name="intelepon" id="intelepon" value="{{ $data[0]['intelepon'] }}" readonly>
+     <div class="form-group row">
+        <label class="col-sm-2">Nomor</label>
+          <div class="col-sm-10">
+            <input type="number" class="form-control" name="intelepon" id="intelepon" value="{{ $data[0]['intelepon'] }}" readonly>
+          </div>
      </div>
 
-    <div class="form-group">
-      <label>Gender</label>
-      <input type="text" class="form-control" name="ingender" id="ingender" value="{{ $data[0]['ingender'] }}" readonly>
+    <div class="form-group row">
+      <label class="col-sm-2">Gender</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="ingender" id="ingender" value="{{ $data[0]['ingender'] }}" readonly>
+        </div>
     </div>
-    <div class="form-group">
-      <label>Alamat</label>
-      <textarea class="form-control" name="inalamat" id="inalamat" rows="3" readonly>{{ $data[0]['inalamat'] }}</textarea>
+
+    <div class="form-group row">
+      <label class="col-sm-2">Alamat</label>
+        <div class="col-sm-10">
+          <textarea class="form-control" name="inalamat" id="inalamat" rows="3" readonly>{{ $data[0]['inalamat'] }}</textarea>
+        </div>
     </div>
 </form>
     </div>
